68d33af80a1dcc12a72fd43b1fdae10bc2ea7086
Willyb15 <william.steen.bryant@gmail.com>
2016-05-10 12:04:49 -0400

    php-into

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+	$name = "Will";
+	print "Hello, " . $name
+
+?> 
+
+<h1> This is an H1 tag inside of my php file </h1>
+
+<?php
+
+	print "<h1> This is an H1 tag inside my php tag </h1>"
+
+?>
